da00749a51ff1ed2f4567c7bc2f79ca860801c34
Kairav Parekh <kparekh01@gmail.com>
2016-09-13 20:35:22 -0400

    initial commit

---
+++ lib/nyc_fire_houses.rb
@@ -0,0 +1,32 @@
+require "nyc_fire_houses/version"
+require "unirest"
+
+module NycFireHouses
+  class FireHouse
+  attr_reader :borough, :facilityaddress, :facilityname
+
+  def initialize(fire_house)
+    @borough = fire_house["borough"]
+    @name = fire_house["facilityname"]
+    @addrress = fire_house["facilityaddress"]
+  end
+
+  def self.all
+      fire_house_array = Unirest.get('https://data.cityofnewyork.us/resource/byk8-bdfw.json').body
+      create_markets(fire_house_array)
+    end
+
+  def self.where(fire_house)
+     key = fire_house.keys.first.to_s
+     value = fire_house.values.first
+     fire_house_array = Unirest.get("https://data.cityofnewyork.us/resource/byk8-bdfw.json?#{key}=#{value}").body
+     create_markets(fire_house_array)
+  end
+
+  private_class_method :create_markets
+
+    def self.create_markets(fire_house_array)
+      fire_house_array.map { |fire_house| FireHouse.new(fire_house)}
+    end
+
+end
---
+++ lib/nyc_fire_houses/version.rb
@@ -0,0 +1,3 @@
+module NycFireHouses
+  VERSION = "0.1.0"
+end
